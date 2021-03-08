294ece8909909e1523bea0fd4f96813d6a954efd
Tim Newton <timothy.newton.cma13@gmail.com>
2021-03-08 12:06:53 -0600

    move lazysetup to `experiements` directory

--- core/defaults.py
+++ core/defaults.py
@@ -1,6 +1,6 @@
-import numpy
+from numpy.core.fromnumeric import sum as numpy_sum
 
 optimization_constraints = {
     'type': 'eq',
-    'fun':  lambda x: numpy.sum(x)-1,
+    'fun':  lambda x: numpy_sum(x)-1,
 }
