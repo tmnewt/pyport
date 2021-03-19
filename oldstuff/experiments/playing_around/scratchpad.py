cd00744eeb3ca42960a2ce11f390c3089d9c84b1
Tim Newton <timothy.newton.cma13@gmail.com>
2021-03-19 18:17:47 -0500

    untrack oldstuff. Look at older commits if you really want them...

--- oldstuff/experiments/playing_around/scratchpad.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-data = pd.Series(np.random.randn(9),
-                 index=[['a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'],
-                        [1, 2, 3, 1, 3, 1, 2, 2, 3]])
-
-print(data.loc['a'])
